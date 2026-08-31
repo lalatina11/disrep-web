c1a00ca445669a142b2838c2eb85d58b0ffb41ee
lalatina11 <rahmadancandra@gmail.com>
2026-08-31 09:47:15 +0700

    refactor: update import path for ModeToggle component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import SignOutForm from "#/components/forms/auth-form/sign-out-form";
-import { ModeToggle } from "#/components/provider/mode-toggle";
+import { ModeToggle } from "#/components/mode-toggle";
 import { Button } from "#/components/ui/button";
 import useUserStore from "#/lib/stores/use-user-store";
 
