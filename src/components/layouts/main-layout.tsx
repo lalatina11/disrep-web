22ad5c25d7da8b693fbe54bfa577202b647d9117
lalatina11 <rahmadancandra@gmail.com>
2026-08-31 16:50:38 +0700

    feat: add className prop to MainLayout for customizable styling

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,12 +1,19 @@
 import type { ReactNode } from "react";
+import { cn } from "#/lib/utils";
 import MainNavbar from "../navbars/main-navbar";
 
 interface Props {
 	children: ReactNode;
+	className?: string;
 }
 const MainLayout = (props: Props) => {
 	return (
-		<div className="flex flex-col gap-4 min-h-screen">
+		<div
+			className={cn(
+				"flex flex-col gap-4 min-h-screen",
+				props.className && props.className,
+			)}
+		>
 			<MainNavbar />
 			{props.children}
 		</div>
