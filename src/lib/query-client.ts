dea3bf0345f5283f4ea978ba1d0d5e7d2d805924
lalatina11 <rahmadancandra@gmail.com>
2026-09-03 18:17:54 +0700

    fix: resolve URL construction error when query parameters are present in query-client

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -7,8 +7,10 @@ const queryClient = new QueryClient({
 			queryFn: ({ queryKey }) => {
 				const [url, params] = queryKey as [string, Record<string, unknown>?];
 
+				const isQueryParamsAvailable = Boolean(params);
+
 				const fullUrl = params
-					? `${url}?${new URLSearchParams(params as Record<string, string>).toString()}`
+					? `${url}?${isQueryParamsAvailable && new URLSearchParams(params as Record<string, string>).toString()}`
 					: url;
 				return clientFetch(fullUrl, "GET");
 			},
