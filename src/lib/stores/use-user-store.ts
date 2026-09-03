77720b627e90dbf84dca01693667745c1f986b77
lalatina11 <rahmadancandra@gmail.com>
2026-09-03 08:25:27 +0700

    feat: add isAdmin method to user store to validate user roles against admin constants

--- src/lib/stores/use-user-store.ts
+++ src/lib/stores/use-user-store.ts
@@ -1,19 +1,28 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
+import { ADMIN_ROLES, type AdminRoleType } from "../constatns/user-role";
 import type { User } from "../types";
 
 interface UseUserStore {
 	user: User | null;
 	setUser: (u: User) => void;
 	clearUser: () => void;
+	isAdmin: () => boolean;
 }
 
 const useUserStore = create(
 	persist<UseUserStore>(
-		(set) => ({
+		(set, get) => ({
 			user: null,
 			clearUser: () => set({ user: null }),
 			setUser: (user) => set({ user }),
+			isAdmin: () => {
+				const { user } = get();
+				if (!user) {
+					return false;
+				}
+				return ADMIN_ROLES.includes(user.role as AdminRoleType);
+			},
 		}),
 		{
 			name: "use-user-store",
